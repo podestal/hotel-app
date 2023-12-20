f311ce12ff2072c26e642f84787c2549c63fb688
podestal <luisrodriguezpodesta@gmail.com>
2023-12-20 11:34:24 -0500

    tags validate/invalidate

--- src/features/api/apiSlice.js
+++ src/features/api/apiSlice.js
@@ -4,16 +4,19 @@ export const apiSlice = createApi(
     {
         reducerPath: 'api',
         baseQuery: fetchBaseQuery({ baseUrl: "http://127.0.0.1:8000/"}),
+        tagTypes: ['Cabins'],
         endpoints: builder => ({
             getCabins: builder.query({
-                query: () => 'api/cabins/'
+                query: () => 'api/cabins/',
+                providesTags: ['Cabins']
             }),
             addCabin: builder.mutation({
                 query: (cabin) => ({
                     url: 'api/cabins/',
                     method: 'POST',
                     body: cabin
-                })
+                }),
+                invalidatesTags: ['Cabins']
             })
         })
     }
